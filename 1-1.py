37c3a90bf794ec7912a2bdb5b7f44af1bba16289
Siena Chen <clairechen6314@gmail.com>
2019-06-05 13:11:01 +0900

    Update 1-1.py

--- 1-1.py
+++ 1-1.py
@@ -23,11 +23,27 @@ print(beer_card)
 deck = FrenchDeck()
 print(len(deck))
 
+#ランダムにカードを選ぶ
 from random import choice
 print(choice(deck))
 
+#トランプ組中のカード確認
 for card in deck:
     print(card)
 
+#カード順を逆
 for card in reversed(deck):
+    print(card)
+
+#カードの有無判断
+print(Card('Q', 'hearts') in deck)
+
+#ルールの強さ
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+for card in sorted(deck, key=spades_high):
     print(card)
